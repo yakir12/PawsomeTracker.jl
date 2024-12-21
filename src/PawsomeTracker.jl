8935339b4aa8f413a15f2ea0f342efe46398f6cd
Yakir Gagnon <12.yakir@gmail.com>
2024-12-21 18:47:01 +0100

    better tests

--- src/PawsomeTracker.jl
+++ src/PawsomeTracker.jl
@@ -27,10 +27,15 @@ function getwindow(window_size)
     return radii, window
 end
 
-function initiate(vid, _, start_xy)
+function initiate(vid, _, start_index::CartesianIndex{2})
+    sz = reverse(out_frame_size(vid))
+    return sz, read(vid), Tuple(start_index)
+end
+
+function initiate(vid, _, start_xy::NTuple{2})
     sz = reverse(out_frame_size(vid))
     x, y = start_xy
-    start_ij = (y, round(Int, x / VideoIO.aspect_ratio(vid)))
+    start_ij = round.(Int, (y, x / VideoIO.aspect_ratio(vid)))
     return sz, read(vid), start_ij
 end
 
@@ -50,29 +55,33 @@ function guess_window_size(target_width)
 end
 
 """
-    track(file; start, stop, target_width, start_xy, window_size)
+    track(file; start, stop, target_width, start_location, window_size)
 
 Use a Difference of Gaussian (DoG) filter to track a target in a video `file`. 
 - `start`: start tracking after `start` seconds. Defaults to 0.
 - `stop`: stop tracking at `stop` seconds.  Defaults to the full duration of the video.
 - `target_width`: the full width of the target (diameter, not radius). It is used as the FWHM of the center Gaussian in the DoG filter. Arbitrarily defaults to 25 pixels.
-- `start_xy`: a tuple (x, y) where x and y are the horizontal and vertical pixel-distances between the left-top corner of the video-frame and the center of the target at `start`. If `start_xy` is `missing`, the target will be detected in a large (half as large as the frame) window centered at the frame. Defaults to `missing`.
+- `start_location`: one of three things:
+    1. `missing`: the target will be detected in a large (half as large as the frame) window centered at the frame.
+    2. `CartesianIndex{2}`: the cartesian index (into the image matrix) indicating where the target is at `start`. Note that when the aspect ratio of the video is not equal to one, this cartesian index should be to the raw, un-scaled, image frame.
+    3. `NTuple{2}`: (x, y) where x and y are the horizontal and vertical pixel-distances between the left-top corner of the video-frame and the target at `start`. Note that regardless of the aspect ratio of the video, this coordinate should be to the scaled image frame (what you'd see in a video player).
+    Defaults to `missing`.
 - `window_size`: a tuple (w, h) where w and h are the width and height of the window (region of interest) in which the algorithm will in to detect the target in the next frame. This should be larger than the `target_width` and relate to how fast the target moves between subsequent frames. Defaults to 1.5 times the target width.
 
-Returns a vector with the time-stamps per frame, a vector of cartesian indices for the detection index per frame, and a function that converts from a cartesian index to a xy coordinate. This function takes into account a pixel aspect ratio that isn't equal to one.
+Returns a vector with the time-stamps per frame and a vector of cartesian indices for the detection index per frame.
 """
 function track(file::AbstractString; 
         start::Real = 0,
         stop::Real = VideoIO.get_duration(file),
         target_width::Real = 25,
-        start_xy::Union{Missing, NTuple{2, Int}} = missing,
+        start_location::Union{Missing, NTuple{2}, CartesianIndex{2}} = missing,
         window_size::NTuple{2, Int} = guess_window_size(target_width)
     )
 
-    openvideo(vid -> _track(vid, start, stop, target_width, start_xy, window_size), file, target_format=VideoIO.AV_PIX_FMT_GRAY8)
+    openvideo(vid -> _track(vid, start, stop, target_width, start_location, window_size), file, target_format=VideoIO.AV_PIX_FMT_GRAY8)
 end
 
-function _track(vid, start, stop, target_width, start_xy, window_size)
+function _track(vid, start, stop, target_width, start_location, window_size)
     read(vid) # needed to get the right time offset t₀
     t₀ = gettime(vid)
     start += t₀
@@ -82,7 +91,7 @@ function _track(vid, start, stop, target_width, start_xy, window_size)
     σ = target_width/2.355
     kernel = -Kernel.DoG(σ)
 
-    sz, img, start_ij = initiate(vid, kernel, start_xy)
+    sz, img, start_ij = initiate(vid, kernel, start_location)
 
     indices = [start_ij]
     ts = [gettime(vid)]
@@ -101,12 +110,12 @@ function _track(vid, start, stop, target_width, start_xy, window_size)
         end
     end
 
-    function index2xy(ij::CartesianIndex)
-        i, j = Tuple(ij)
-        return (j * VideoIO.aspect_ratio(vid), i)
-    end
+    # function index2xy(ij::CartesianIndex)
+    #     i, j = Tuple(ij)
+    #     return (j * VideoIO.aspect_ratio(vid), i)
+    # end
 
-    return ts .- t₀, CartesianIndex.(indices), f
+    return ts .- t₀, CartesianIndex.(indices)
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -27,61 +27,93 @@ function generate(w, h, target_width, start_ij, file)
     return org
 end
 
-compare() = mktempdir() do path
-    file = joinpath(path, "example.mkv")
-    w, h = (200, 150)
-    start_ij = (rand(h ÷ 4 + 1 : 3h ÷ 4 - 1), rand(w ÷ 4 + 1 : 3w ÷ 4 - 1))
-    target_width = rand(5:20)
-    org = generate(w, h, target_width, start_ij, file)
-    start_xy = rand(Bool) ? reverse(start_ij) : missing
-    _, _, tracked = track(file; start_xy)
-    sqrt(mean([LinearAlgebra.norm_sqr(o .- reverse(t)) for (o, t) in zip(org, tracked)]))
-end
+# compare() = mktempdir() do path
+#     file = joinpath(path, "example.mkv")
+#     w, h = (200, 150)
+#     start_ij = (rand(h ÷ 4 + 1 : 3h ÷ 4 - 1), rand(w ÷ 4 + 1 : 3w ÷ 4 - 1))
+#     target_width = rand(5:20)
+#     org = generate(w, h, target_width, start_ij, file)
+#     start_location = rand((missing, CartesianIndex(start_ij), reverse(start_ij)))
+#     _, _, tracked = track(file; start_location)
+#     sqrt(mean([LinearAlgebra.norm_sqr(o .- reverse(t)) for (o, t) in zip(org, tracked)]))
+# end
 
-compare_aspect() = mktempdir() do path
+compare() = mktempdir() do path
+
+    # path = mktempdir("/home/yakir/PawsomeTracker.jl/test"; cleanup = false)
     file = joinpath(path, "example.mkv")
-    w, h = (200, 200)
-    start_ij = (75, 50)
+
+    aspect = rand(1:3)
+    w = rand((100, 150, 200))
+    w2 = w ÷ aspect
+    w = isodd(w2) ? (w2 + 1)*aspect : w
+    h = rand((100, 150, 200))
+    i = h÷2 + rand(-20:20)
+    j = w÷2 + rand(-20:20)
+    start_location = rand((missing, CartesianIndex(i, j), (j, i)))
+    start_ij = if ismissing(start_location) 
+        (h÷2, w÷2) 
+        elseif start_location isa CartesianIndex
+            Tuple(start_location)
+        else
+            reverse(start_location)
+        end
     target_width = rand(5:20)
     org = generate(w, h, target_width, start_ij, file)
     file2 = joinpath(path, "example2.mkv")
-    a = 2
-    w2 = w ÷ a
-    run(`$(FFMPEG.ffmpeg()) -hide_banner -loglevel error -i $file -vf scale=$w2:$h,setdar=1:$a,setsar=1:$a -aspect 1:1 -c:v libx264 $file2`)
-    _, _, tracked = track(file2; start_xy = reverse(start_ij))
-    sqrt(mean([LinearAlgebra.norm_sqr(o .- reverse(t)) for (o, t) in zip(org, tracked)]))
+    w2 = w ÷ aspect
+    run(`$(FFMPEG.ffmpeg()) -y -hide_banner -loglevel error -i $file -vf scale=$w2:$h,setsar=$aspect -c:v libx264 $file2`)
+    # run(`$(FFMPEG.ffmpeg()) -y -hide_banner -loglevel error -i $file -vf scale=$w2:$h,setdar=1:$a,setsar=1:$a -aspect 1:$a -c:v libx264 $file2`)
+    openvideo(VideoIO.aspect_ratio, file2)
+    fix_start_location(x) = x
+    function fix_start_location(ij::CartesianIndex{2})
+        i, j = Tuple(ij)
+        CartesianIndex(i, j ÷ aspect)
+    end
+    _, tracked = track(file2; start_location = fix_start_location(start_location))
+    function scale(ij::CartesianIndex{2})
+        i, j = Tuple(ij)
+        (i, aspect*j)
+    end
+    sqrt(mean([LinearAlgebra.norm_sqr(o .- scale(t)) for (o, t) in zip(org, tracked)]))
+
+
+
+    # file3 = joinpath(path, "example3.mkv")
+    # vid = openvideo(file2)
+    # open_video_out(file3, RGB.(read(vid)), framerate=24) do writer
+    #     for (img, ij) in zip(vid, tracked)
+    #         frame = draw(RGB.(img), CirclePointRadius(Point(ij), target_width ÷ 4), RGB(1,0,0))
+    #         write(writer, frame)
+    #     end
+    # end
+
+
 end
 
+# n = 100
+# x = zeros(n)
+# Threads.@threads for i in 1:n
+#     x[i] = compare()
+# end
+# @show x
 
 
 @testset "PawsomeTracker.jl" begin
     @testset "Code quality (Aqua.jl)" begin
         Aqua.test_all(PawsomeTracker; ambiguities = VERSION ≥ VersionNumber("1.7"))
     end
-    @testset "multiple random trajectories" begin
+    @testset "random trajectories" begin
         for _ in 1:80
-            @test compare() < 1
-        end
-    end
-
-    @testset "video with aspect ration ≠ 1" begin
-        for _ in 1:80
-            @test compare_aspect() < 2
+            @test compare() < 2
         end
     end
 
     @testset "concurrency" begin
-        @testset "multiple random trajectories" begin
+        @testset "random trajectories" begin
             Threads.@threads for _ in 1:80
-                @test compare() < 1
+                @test compare() < 2
             end
         end
-
-        @testset "video with aspect ration ≠ 1" begin
-            Threads.@threads for _ in 1:80
-                @test compare_aspect() < 2
-            end
-        end
-
     end
 end
