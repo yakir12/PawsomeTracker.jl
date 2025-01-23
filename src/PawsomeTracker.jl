4de742c35b18ac2d029c1f00634feaf4a75bb2a4
Yakir Gagnon <12.yakir@gmail.com>
2025-01-23 22:41:26 +0100

    wip

--- src/PawsomeTracker.jl
+++ src/PawsomeTracker.jl
@@ -9,7 +9,12 @@ using OffsetArrays: OffsetArrays
 using PaddedViews: PaddedViews, PaddedView
 using StatsBase: StatsBase, mode
 using FFMPEG_jll: ffmpeg, ffprobe
-using VideoIO: openvideo, AV_PIX_FMT_GRAY8, aspect_ratio
+using VideoIO: openvideo, AV_PIX_FMT_GRAY8, aspect_ratio, open_video_out, VideoWriter, close_video_out!
+using ImageDraw: draw!, CirclePointRadius
+using FreeTypeAbstraction: renderstring!, findfont, FTFont
+using ColorTypes: Gray
+using FixedPointNumbers: N0f8
+using ImageTransformations: imresize!
 
 export track
 
@@ -83,20 +88,77 @@ function track(file::AbstractString;
         stop::Real = get_duration(file),
         target_width::Real = 25,
         start_location::Union{Missing, NTuple{2}, CartesianIndex{2}} = missing,
-	window_size::Union{Int, NTuple{2, Int}} = guess_window_size(target_width),
+        window_size::Union{Int, NTuple{2, Int}} = guess_window_size(target_width),
         darker_target::Bool = true,
-	fps::Real = get_fps(file)
+        fps::Real = get_fps(file),
+        diagnostic_file::Union{Nothing, AbstractString} = nothing
     )
 
     ts = range(start, stop; step = 1/fps)
     n = length(ts)
     t = stop - start
     cmd = `$(ffmpeg()) -loglevel 8 -ss $start -i $file -t $t -r $fps -preset veryfast -f matroska -`
-    ij = openvideo(vid -> _track(vid, n, target_width, start_location, fix_window_size(window_size), darker_target), open(cmd), target_format=AV_PIX_FMT_GRAY8)
+    ij = openvideo(vid -> _track(vid, n, target_width, start_location, fix_window_size(window_size), darker_target, diagnostic_file), open(cmd), target_format=AV_PIX_FMT_GRAY8)
     return ts, CartesianIndex.(ij)
 end
 
-function _track(vid, n, target_width, start_location, window_size, darker_target)
+function findfirstfont()
+    for c in 'a':'z'
+        face = findfont(string(c))
+        if !isnothing(face)
+            return face
+        end
+    end
+    return nothing
+end
+
+struct Diagnose
+    label::String
+    face::FTFont
+    writer::VideoWriter
+    buffer::Matrix{Gray{N0f8}}
+    ratio::Int
+
+    function Diagnose(file, img)
+        label = first(splitext(basename(file)))
+        ratio = 8
+        sz = size(img) .÷ ratio
+        sz = 2 .* round.(Int, sz ./ 2)
+        buffer = Matrix{Gray{N0f8}}(undef, sz...)
+        writer = open_video_out(file, buffer)
+        face = findfirstfont()
+        new(label, face, writer, buffer, ratio)
+    end
+end
+
+struct Dont end
+
+diagnose(file, img) = Diagnose(file, img)
+diagnose(::Nothing, img) = Dont()
+
+
+function diagnose(f, file, img)
+    dia = diagnose(file, img)
+    try
+        f(dia)
+    finally
+        close(dia)
+    end
+end
+
+Base.close(dia::Diagnose) = close_video_out!(dia.writer)
+Base.close(::Dont) = nothing
+
+function (dia::Diagnose)(img, point)
+    imresize!(dia.buffer, img)
+    draw!(dia.buffer, CirclePointRadius(CartesianIndex(point .÷ dia.ratio), 2))
+    renderstring!(dia.buffer, dia.label, dia.face, 10, 10, 10, halign=:hleft, valign = :vtop)
+    write(dia.writer, dia.buffer)
+end
+(::Dont)(_, _) = nothing
+
+
+function _track(vid, n, target_width, start_location, window_size, darker_target, diagnostic_file)
     σ = target_width/2sqrt(2log(2))
     kernel = darker_target ? -Kernel.DoG(σ) : Kernel.DoG(σ)
 
@@ -112,17 +174,18 @@ function _track(vid, n, target_width, start_location, window_size, darker_target
     fillvalue = mode(img)
     pimg = PaddedView(fillvalue, img, window_indices)
 
-    for i in 2:n
-        read!(vid, pimg.data)
-        indices[i] = getnext(indices[i - 1], pimg , window, kernel, sz)
+    diagnose(diagnostic_file, img) do dia
+        for i in 2:n
+            read!(vid, pimg.data)
+            indices[i] = getnext(indices[i - 1], pimg , window, kernel, sz)
+            dia(pimg.data, indices[i])
+        end
     end
 
     return indices
 end
 
-
-
-
+# TODO: if I'm resizeing evrything, might as well track the smaller image, 
 
 
 
