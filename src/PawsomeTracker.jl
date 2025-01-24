ac935a35f7eb209d4000e1eaef635bdda165dca2
Yakir Gagnon <12.yakir@gmail.com>
2025-01-24 08:12:56 +0100

    wip

--- src/PawsomeTracker.jl
+++ src/PawsomeTracker.jl
@@ -3,7 +3,7 @@ module PawsomeTracker
 # using LinearAlgebra
 # using VideoIO, OffsetArrays, ImageFiltering, PaddedViews, StatsBase
 
-using ImageFiltering: ImageFiltering, Kernel, imfilter
+using ImageFiltering: ImageFiltering, Kernel, imfilter!, Algorithm, NoPad
 using LinearAlgebra: LinearAlgebra
 using OffsetArrays: centered, OffsetMatrix
 using PaddedViews: PaddedViews, PaddedView
@@ -11,68 +11,70 @@ using StatsBase: StatsBase, mode
 using FFMPEG_jll: ffmpeg, ffprobe
 using VideoIO: openvideo, AV_PIX_FMT_GRAY8, aspect_ratio
 using ColorTypes: Gray
+using FixedPointNumbers: N0f8
+using ComputationalResources: CPUThreads
 
 export track
 
-include("ffmpeg.jl")
+# include("ffmpeg.jl")
 
 struct Tracker
     sz::Tuple{Int64, Int64}
-    window::CartesianIndices{2, Tuple{UnitRange{Int64}, UnitRange{Int64}}}
+    radii::Tuple{Int64, Int64}
     kernel::OffsetMatrix{Float64, Matrix{Float64}}
-    buff::Matrix{Gray{Float64}}
+    img::PaddedView{Gray{N0f8},2,Tuple{Base.IdentityUnitRange{UnitRange{Int64}},Base.IdentityUnitRange{UnitRange{Int64}}},PermutedDimsArray{Gray{N0f8}, 2, (2, 1), (2, 1), Matrix{Gray{N0f8}}}}
+    buff::OffsetMatrix{Float64, Matrix{Float64}}
 
-    function Tracker(kernel, sz, window)
-        buff = Matrix{ColorTypes.Gray{Float64}}(undef, size(window)) 
-        new(sz, window, kernel, buff)
+    function Tracker(_img, target_width, window_size, darker_target)
+        sz = size(_img)
+        σ = target_width/2sqrt(2log(2))
+        direction = darker_target ? -1 : +1
+        kernel = direction*Kernel.DoG(σ)
+        radii = window_size .÷ 2
+        h = radii .+ size(kernel)
+        pad_indices = UnitRange.(1 .- h, sz .+ h)
+        fillvalue = mode(_img)
+        img = PaddedView(fillvalue, _img, pad_indices)
+        _buff = Matrix{Float64}(undef, length.(pad_indices)) 
+        buff = OffsetMatrix(_buff, pad_indices)
+        return new(sz, radii, kernel, img, buff)
     end
 end
 
-
-
-
-function (tracker::Tracker)(guess, img)
-    frame = centered(img, guess)[tracker.window]
-    imfilter!(tracker.buff, frame, tracker.kernel)
-    _, i = findmax(tracker.buff)
-    guess = guess .+ Tuple(tracker.window[i])
-    return min.(max.(guess, (1, 1)), sz)
+function (trckr::Tracker)(guess)
+    window_indices = UnitRange.(guess .- trckr.radii, guess .+ trckr.radii)
+    imfilter!(CPUThreads(Algorithm.FIR()), trckr.buff, trckr.img, trckr.kernel, NoPad(), window_indices)
+    # # imfilter!(CPU1(Algorithm.FIR()), trckr.buff, trckr.img, trckr.kernel, NoPad(), window_indices)
+    v = view(trckr.buff, window_indices...)
+    _, ij = findmax(v)
+    guess = getindex.(parentindices(v), Tuple(ij))
+    return min.(max.(guess, (1, 1)), trckr.sz)
 end
 
 
 function guess_window_size(target_width)
     σ = target_width/2sqrt(log(2))
-    l = 4ceil(Int, σ) + 1 # calculates the size of the DoG kernel
-    return (l, l)
+    return 4ceil(Int, σ) + 1 # calculates the size of the DoG kernel
 end
 
-fix_window_size(wh::NTuple{2, Int}) = reverse(wh)
-
-fix_window_size(l::Int) = (l, l)
-
-function getwindow(window_size)
-    radii = window_size .÷ 2
-    wr = CartesianIndex(radii)
-    window = -wr:wr
-    return radii, window
-end
-
-function initiate(start_index::CartesianIndex{2}, _, _, _, _)
-    return Tuple(start_index)
-end
-
-function initiate(start_xy::NTuple{2}, vid, _, _, _)
-    x, y = start_xy
-    sar = aspect_ratio(vid)
-    start_ij = round.(Int, (y, x / sar))
-    return start_ij
-end
-
-function initiate(::Missing, _, img, sz, kernel)
-    guess = sz .÷ 2
-    _, initial_window = getwindow(sz .÷ 2)
-    start_ij = getnext(guess, img, initial_window, kernel, sz)
-    return start_ij
+function get_best_stream(file, start, stop, fps)
+    t = stop - start
+    # try
+    #     cmd = `$(ffmpeg()) -loglevel 8 -ss $start -i $file -t $t -r $fps -vcodec copy -f m4v -an -` # m4v
+    #     vid = openvideo(open(cmd), target_format=AV_PIX_FMT_GRAY8) # 0.7 seconds
+    # catch ex1
+    #     try 
+    #         cmd = `$(ffmpeg()) -loglevel 8 -ss $start -i $file -t $t -r $fps -vcodec libx264 -crf 51 -preset ultrafast -f m4v -an -` # m4v
+    #         vid = openvideo(open(cmd), target_format=AV_PIX_FMT_GRAY8) # 0.7 seconds
+    #     catch ex2
+            # cmd = `$(ffmpeg()) -loglevel 8 -ss $start -i $file -t $t -r $fps -vcodec libx264 -crf 40 -preset ultrafast -f avi -an -` # m4v
+            cmd = `$(ffmpeg()) -loglevel 8 -ss $start -i $file -t $t  -vf scale=960:-1 -r $fps -vcodec libx264 -crf 40 -preset ultrafast -f matroska -an -` # m4v
+            cmd = `$(ffmpeg()) -loglevel 8 -ss $start -i $file -t $t  -vf scale=960:-1 -pix_fmt gray -f yuv4mpegpipe -r $fps -an -` # m4v
+            # cmd = `$(ffmpeg()) -loglevel 8 -ss $start -i $file -t $t -r $fps -vcodec libx264 -crf 40 -preset ultrafast -f m4v -an -` # m4v
+            # vid = openvideo(open(cmd), target_format=AV_PIX_FMT_GRAY8) # 0.7 seconds
+            vid = openvideo(open(cmd)) # 0.7 seconds
+        # end
+    # end
 end
 
 """
@@ -80,7 +82,7 @@ end
 
 Use a Difference of Gaussian (DoG) filter to track a target in a video `file`. 
 - `start`: start tracking after `start` seconds. Defaults to 0.
-- `stop`: stop tracking at `stop` seconds.  Defaults to the full duration of the video.
+- `stop`: stop tracking at `stop` seconds. Defaults to a maximum of 86399.999 seconds, equals to 23:59:59.999. In case the video is shorter, tracking will stop at the end of the video file.
 - `target_width`: the full width of the target (diameter, not radius). It is used as the FWHM of the center Gaussian in the DoG filter. Arbitrarily defaults to 25 pixels.
 - `start_location`: one of the following:
     1. `missing`: the target will be detected in a large (half as large as the frame) window centered at the frame.
@@ -91,57 +93,66 @@ Use a Difference of Gaussian (DoG) filter to track a target in a video `file`.
     1. `NTuple{2}`: a tuple (w, h) where w and h are the width and height of the window (region of interest) in which the algorithm will try to detect the target in the next frame. This should be larger than the `target_width` and relate to how fast the target moves between subsequent frames. 
     2. `Int`: both the width and height of the window (region of interest) in which the algorithm will try to detect the target in the next frame. This should be larger than the `target_width` and relate to how fast the target moves between subsequent frames. 
 - `darker_target`: set to `true` if the target is darker than its background, and vice versa. Defaults to `true`.
-- `fps`: frames per second. Sets how many times the target's location is registered per second. Set to a low number for faster and sparser tracking, but adjust the `window_size` accordingly. Defaults to the actual frame rate of the video.
+- `fps`: frames per second. Sets how many times the target's location is registered per second. Set to a low number for faster and sparser tracking, but adjust the `window_size` accordingly. Defaults to 25 frames per second.
 
 Returns a vector with the time-stamps per frame and a vector of Cartesian indices for the detection index per frame.
 """
 function track(file::AbstractString; 
         start::Real = 0,
-        stop::Real = get_duration(file),
+        stop::Real = 86399.999,
         target_width::Real = 25,
         start_location::Union{Missing, NTuple{2}, CartesianIndex{2}} = missing,
-	window_size::Union{Int, NTuple{2, Int}} = guess_window_size(target_width),
+        window_size::Union{Int, NTuple{2, Int}} = guess_window_size(target_width),
         darker_target::Bool = true,
-	fps::Real = get_fps(file)
+        fps::Real = 25
     )
 
     ts = range(start, stop; step = 1/fps)
     n = length(ts)
-    t = stop - start
-    cmd = `$(ffmpeg()) -loglevel 8 -ss $start -i $file -t $t -r $fps -preset veryfast -f matroska -`
-    vid = openvideo(open(cmd), target_format=AV_PIX_FMT_GRAY8) 
-    ij = _track(vid, n, target_width, start_location, fix_window_size(window_size), darker_target)
-    close(vid)
-    return ts, CartesianIndex.(ij)
-end
 
-function _track(vid, n, target_width, start_location, window_size, darker_target)
-    
-    σ = target_width/2sqrt(2log(2))
-    kernel = darker_target ? -Kernel.DoG(σ) : Kernel.DoG(σ)
+    vid = get_best_stream(file, start, stop, fps)
+    # vid = openvideo(file, target_format=AV_PIX_FMT_GRAY8) # 0.7 seconds
+    # seek(vid, start)
 
     img = read(vid)
-    sz = size(img)
-    start_ij = initiate(start_location, vid, img, sz, kernel)
-    
-    indices = Vector{NTuple{2, Int}}(undef, n)
-    indices[1] = start_ij
-    
-    wr, window = getwindow(window_size)
-    window_indices = UnitRange.(1 .- wr, sz .+ wr)
-    fillvalue = mode(img)
-    pimg = PaddedView(fillvalue, img, window_indices)
-    
-    tracker = Tracker(kernel, sz, window)
 
-    for i in 2:n
-        read!(vid, pimg.data)
-        indices[i] = tracker(indices[i - 1], pimg)
+    indices = Vector{NTuple{2, Int}}(undef, n)
+    indices[1] = get_start_ij(start_location, vid, target_width, darker_target, img) # depends on `window_size = sz .÷ 4`
+
+    trckr = Tracker(img, target_width, fix_window_size(window_size), darker_target) # 0.5 seconds
+
+    i = 1
+    while !eof(vid) && i < n
+        i += 1
+        read!(vid, trckr.img.data)
+        indices[i] = trckr(indices[i - 1])
     end
 
-    return indices
+    close(vid)
+
+    return ts[1:i], CartesianIndex.(indices[1:i])
 end
 
+fix_window_size(wh::NTuple{2, Int}) = reverse(wh)
+
+fix_window_size(l::Int) = (l, l)
+
+function get_start_ij(start_index::CartesianIndex{2}, _, _, _, _)
+    return Tuple(start_index)
+end
+
+function get_start_ij(start_xy::NTuple{2}, vid, _, _, _)
+    sar = aspect_ratio(vid)
+    x, y = start_xy
+    return round.(Int, (y, x / sar))
+end
+
+function get_start_ij(::Missing, _, target_width, darker_target, img)
+    sz = size(img)
+    window_size = sz .÷ 4 # this greatly affects processing time!
+    trckr = Tracker(img, target_width, window_size, darker_target) # 0.5 seconds
+    return trckr(trckr.sz .÷ 2)
+end
 
 
 
