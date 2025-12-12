026d6992873edf16da3827c08cc7e4698865b470
Yakir Gagnon <12.yakir@gmail.com>
2025-12-12 14:22:49 +0100

    wip

--- test/test-basic-test.jl
+++ test/test-basic-test.jl
@@ -10,19 +10,19 @@ end
 end
 
 @testitem "Basic functionality test" tags = [:unit, :fast] setup = [SharedData] begin
-    result = sample_input
+    result = SharedData.sample_input
     @test result == expected_output
     @test isa(result, String)
 end
 
 @testitem "Input validation test" tags = [:unit, :validation] setup = [CommonHelpers] begin
-    result = sample_input
+    result = SharedData.sample_input
     @test CommonHelpers.is_valid_string(result)
 end
 
 @testitem "Performance test" tags = [:integration, :slow] begin
     # Test that function executes quickly
-    result = @timed sample_input
+    result = @timed SharedData.sample_input
     @test result.time < 0.001  # Should complete in less than 1ms
     @test result.value == "Hello, World!"
 end
